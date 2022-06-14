d7525032e7a0e58fca22e68f332067030bd37492
RenanLeviathan <renan.leviathan@gmail.com>
2022-06-14 16:08:04 -0300

    3 files modified, 1 added

--- App.tsx
+++ App.tsx
@@ -6,10 +6,20 @@ import Footer from './Footer';
 import FullPageNav from './FullPageNav';
 
 export default function App() {
+  const [countries, setCountries] = React.useState([]);
+  React.useEffect(() => {
+    fetch('./countries.json', {
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((res) => setCountries(res['data']));
+  });
   return (
     <div>
       <Menu />
-      <FullPageNav />
+      <FullPageNav cnt={countries} />
       <Footer />
     </div>
   );
